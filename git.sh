1db3a4a840530a1a341e7fe83544303373cb1902
aicanhelp <modongsong@126.com>
2023-10-14 15:36:36 +0800

    Remove a submodule resources/predictionio

--- git.sh
+++ git.sh
@@ -3,36 +3,44 @@
 cur_dir=`pwd`
 
 function do_del() {
-    MOD_NAME=$1
+  local MOD_NAME=${1:?'[ERROR]argument name required!'}
 
-   echo "deinit ${MOD_NAME} "
+  echo "deinit ${MOD_NAME} "
   # 逆初始化模块，其中{MOD_NAME}为模块目录，执行后可发现模块目录被清空
-  git submodule deinit resources/${MOD_NAME}
+  git submodule deinit ${MOD_NAME}
 
-  echo "remove ${MOD_NAME} "
+  echo "remove ${MOD_NAME}"
   # 删除.gitmodules中记录的模块信息（--cached选项清除.git/modules中的缓存）
-  git rm -f --cached resources/${MOD_NAME}
+  git rm -f --cached ${MOD_NAME}
 
-  rm -rf .git/module/resources/${MOD_NAME}
-  rm -rf resources/${MOD_NAME}
+  rm -rf .git/module/${MOD_NAME}
+  rm -rf ${MOD_NAME}
 
   # 提交更改到代码库，可观察到'.gitmodules'内容发生变更
-  git commit -am "Remove a submodule resources/${MOD_NAME}"
+  git commit -am "Remove a submodule ${MOD_NAME}"
   git push
 }
 
 function do_add() {
-    git submodule add $1 resources/$2
+   local repo=${1:?'[ERROR]argument repo required!'}
+   local type=${2:?'[ERROR]argument type required!'}
+
+   local name=`echo ${repo} | sed 's/.*\///' | sed 's/\..*//'`
+   git submodule add ${repo} ${type}-projects/$name
 }
 
 function do_list() {
-    git submodule|awk '{print $2}'|sed 's/resources\///'
+    git submodule|awk '{print $2}'
 }
 
 function do_update(){
-   cd resources/$1
-   git pull
-   cd ${cur_dir}
+  local type=${1:?'[ERROR]argument type required!'}
+  local name=${2:?'[ERROR]argument name required!'}
+
+  MOD_NAME=$1-projects/$2
+  cd ${MOD_NAME}
+  git pull
+  cd ${cur_dir}
 }
 
 function do_update_all(){
@@ -40,8 +48,14 @@ function do_update_all(){
 }
 
 function do_mv(){
-  mkdir -p resources/$2
-  git mv resources/$1 resources/$2/$1
+  local src=${1:?'[ERROR]argument src required!'}
+  local type=${2:?'[ERROR]argument target_type required!'}
+
+  local name=`echo ${src} | sed 's/.*\///'`
+
+  MOD_NAME=$type-projects/${name}
+  mkdir -p ${type}-projects1
+  git mv ${src} ${MOD_NAME}
 }
 
-do_$1 $2 $3
+do_$1 $2 $3 $4
