71630aa118300c9bc7fb7da5d3f2396c9f213829
aicanhelp <modongsong@126.com>
2024-11-14 20:05:11 +0800

    Updated:

--- git.sh
+++ git.sh
@@ -7,13 +7,15 @@ function do_del() {
 
   echo "deinit ${MOD_NAME} "
   # 逆初始化模块，其中{MOD_NAME}为模块目录，执行后可发现模块目录被清空
-  git submodule deinit ${MOD_NAME}
+  git submodule deinit -f ${MOD_NAME}
+
 
   echo "remove ${MOD_NAME}"
   # 删除.gitmodules中记录的模块信息（--cached选项清除.git/modules中的缓存）
+  git config -f .gitmodules --remove-section submodule.submodule_path
   git rm -f --cached ${MOD_NAME}
 
-  rm -rf .git/module/${MOD_NAME}
+  rm -rf .git/modules/${MOD_NAME}
   rm -rf ${MOD_NAME}
 
   # 提交更改到代码库，可观察到'.gitmodules'内容发生变更
@@ -24,14 +26,22 @@ function do_del() {
 function do_add() {
    local repo=${1:?'[ERROR]argument repo required!'}
    local type=${2:?'[ERROR]argument type required!'}
-   local name=$3
+   local sub=$3
 
-   test -z $name && name=`echo ${repo} | sed 's/.*\///' | sed 's/\..*//'`
+   name=`echo ${repo} | sed 's/.*\///' | sed 's/\..*//'`
+   project_dir=${type}-projects
+   ! test -z $sub && project_dir=${project_dir}/${sub}
+
+   MOD_NAME=${project_dir}/${name}
+   mkdir -p ${project_dir}
    git submodule add ${repo} ${type}-projects/$name
 }
 
 function do_list() {
-    git submodule|awk '{print $2}'
+   type=$1
+   
+   test -z $type && ls | grep '\-projects' |sed 's/-projects//' && exit 0
+   git submodule status ${type}-projects|awk '{print $2}'
 }
 
 function do_update(){
@@ -39,16 +49,31 @@ function do_update(){
   local name=${2}
 
   local dirs="${name}"
-  test -z $dirs && dirs=`ls ${type}-projects`
+  test -z $dirs && dirs=`ls -d ${type}-projects/*/`
   
   for dir in $dirs
   do
-      MOD_NAME=$1-projects/${dir}
-      cd ${MOD_NAME}
-      git pull
+      cd ${dir}
+      if test -e .git; then
+        echo "Updating ${dir}"
+        git pull
+      else
+        sub_dirs=`ls -d */`
+        for sub_dir in ${sub_dirs}
+        do
+           cd $sub_dir
+           if test -e .git; then
+              echo "Updating ${dir}${sub_dir}"
+              git pull
+           fi
+           cd ..
+        done
+      fi
       cd ${cur_dir}
   done
+
 }
+
 
 function do_update_all(){
    git submodule update --recursive
@@ -57,11 +82,14 @@ function do_update_all(){
 function do_mv(){
   local src=${1:?'[ERROR]argument src required!'}
   local type=${2:?'[ERROR]argument target_type required!'}
+  local sub=$3
 
   local name=`echo ${src} | sed 's/.*\///'`
+  project_dir=${type}-projects
+  ! test -z $sub && project_dir=${project_dir}/${sub}
 
-  MOD_NAME=$type-projects/${name}
-  mkdir -p ${type}-projects
+  MOD_NAME=${project_dir}/${name}
+  mkdir -p ${project_dir}
   git mv ${src} ${MOD_NAME}
 }
 
